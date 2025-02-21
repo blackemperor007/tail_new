b55d05a34bd7cb59f099c5628e5bb899c9e2b07b
Pagnol_wesley <emperorb007@gmail.com>
2025-02-21 10:39:21 +0100

    mise à jour leasing fichier

--- src/index.php
+++ src/index.php
@@ -36,7 +36,7 @@
 
 
     <!------start navbar-->
-    <header class="w-full fixed z-20 bg-white">
+    <header class="w-full fixed z-20 bg-white shadow-[0_2px_15px_-3px_rgba(0,0,0,0.07),0_10px_20px_-2px_rgba(0,0,0,0.04)]">
         <div class="w-full items-center flex justify-between h-14 px-4 shadow xl:shadow-none">
             <div class="flex space-x-8 items-center">
                 <div class="w-24">
